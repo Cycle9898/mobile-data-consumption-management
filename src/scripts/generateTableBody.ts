79f0d6fe39b5705a1918cb7c40c1fa7d967c7eb8
Cycle9898 <cycle9898@gmail.com>
2024-03-25 18:14:31 +0100

    adding style to results section

--- src/scripts/generateTableBody.ts
+++ src/scripts/generateTableBody.ts
@@ -18,12 +18,15 @@ export function generateTableBody(daysObjArray: DaysObjType[], avgDataConsumptio
 		const dataConsumptionNb = (index + 1) * avgDataConsumption;
 
 		const tableRow = document.createElement("tr");
+		tableRow.classList.add("even:bg-cyan-500", "hover:bg-cyan-300", "text-lg");
 
 		const dayCell = document.createElement("td");
+		dayCell.classList.add("py-1");
 		dayCell.innerText = `${daysObj.dayNb} ${daysObj.month}`;
 		tableRow.appendChild(dayCell);
 
 		const dataConsumptionCell = document.createElement("td");
+		dataConsumptionCell.classList.add("py-1");
 		dataConsumptionCell.innerText = `${dataConsumptionNb.toLocaleString("fr-FR", {
 			maximumFractionDigits: 2,
 			minimumFractionDigits: 2
